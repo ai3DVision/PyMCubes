74712a19e0aba7e75bd3f48d372af3e6d73eaaad
David Stutz <dstutz@greenland.mpi-inf.mpg.de>
2017-11-28 10:51:50 +0100

    Forget import statements.

--- mcubes/__init__.py
+++ mcubes/__init__.py
@@ -1,3 +1,3 @@
 
-from ._mcubes import marching_cubes, marching_cubes_func
+from ._mcubes import marching_cubes, marching_cubes2, marching_cubes3, marching_cubes_func
 from .exporter import export_mesh, export_obj, export_off
